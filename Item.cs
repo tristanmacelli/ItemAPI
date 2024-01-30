2ec210c6a90ea3dcd07e16e51139ed98dd83be5c
Tristan Macelli <tristanmacelli@gmail.com>
2024-01-29 18:41:53 -0600

    [REMOVE] unnecessary field annotations and deprecated files

--- Item.cs
+++ Item.cs
@@ -1,13 +1,9 @@
 
 using MongoDB.Bson;
 using MongoDB.Driver;
-using Newtonsoft.Json;
-using MongoDB.Driver.Linq;
 
 public class Item
 {
-    // [BsonId]
-    // [JsonConverter(typeof(StringToObjectId))]
     public ObjectId Id { get; set; }
     public string? Name { get; set; }
     public bool InStock { get; set; }
--- ItemDB.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-class ItemDb : DbContext
-{
-    public ItemDb(DbContextOptions<ItemDb> options)
-        : base(options) { }
-
-    public DbSet<Item> Items => Set<Item>();
-}
